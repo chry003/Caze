589bc199c10aafeb1b5e7f9c441cf8074eff1c0c
rahul <rahul003chry@gmail.com>
2021-08-13 22:51:02 +0530

    cmake

--- CMakeFiles/Caze.dir/Caze-Core/src/graphics/batchrenderer2D.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Caze.dir/Caze-Core/src/graphics/batchrenderer2D.cpp.o: \
- /home/rahul/Development/c++/Caze/Caze-Core/src/graphics/batchrenderer2D.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/Caze.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Caze.
-# This may be replaced when dependencies are built.
